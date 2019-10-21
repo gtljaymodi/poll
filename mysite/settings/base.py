1932c5ad1dcd76482b0d4e837ea5dd44fc41233a
Jay Modi <jay.modi@gatewaytechnolabs.com>
2019-10-21 19:34:03 +0530

    Update settings
    Isolate local and base settings.

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -15,19 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lgwp*n*hpzkulajn_ni9*m=t+q%edlj%z835v6x^@z*%3ng-h^'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
